3815739504a3a8ee0c47bd250a9e37057329403f
Shaik Nada <shaiknada11@gamil.com>
2025-03-02 21:03:42 +0530

    vivitsu-new

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,16 @@ import AboutUs from './pages/AboutUs.jsx';
 import Navbar from './components/Navbar.jsx';
 import Faqs from './components/Faqs.jsx';
 import EventHighlight from './components/EventHighlight.jsx';
-
+import VivitsuMain from './pages/VivitsuMain.jsx';
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <Navbar/>
-        <AboutUs/>
+        {/* <AboutUs/>
         <EventHighlight/>
-        <Faqs/>
+        <Faqs/> */}
+        <VivitsuMain/>
       </div>
     </BrowserRouter>
   )
