a6dd29a396c702ffe100b843517f0f38f10c0646
Subham <subhamkumarshee@gmail.com>
2025-12-26 23:40:58 +0530

    removed sponsers

--- src/pages/VivitsuMain.jsx
+++ src/pages/VivitsuMain.jsx
@@ -8,26 +8,24 @@ import Timeline from "../components/timeline";
 import PortalEffectSlider from "../components/portaleffectslider";
 import VivitsuDetails from "../components/vivitsu/VivitsuDetails";
 
-export default function VivitsuMain(){
-    return(
+export default function VivitsuMain() {
+    return (
         <div>
-        <Vivitsu/>
-        <VivitsuDetails />
-        <Highlights/>
-        {/* <ScrollingText/> */}
-        {/* <Domains/> */}
-        <div className="container mx-auto px-4 py-20 relative z-10">
-        <h2 className="text-6xl font-extrabold text-center mb-12 text-white drop-shadow-[0_0_20px_#00BFFF]">
-  Domains
-</h2>
+            <Vivitsu />
+            <VivitsuDetails />
+            <Highlights />
+            {/* <ScrollingText/> */}
+            {/* <Domains/> */}
+            <div className="container mx-auto px-4 py-20 relative z-10">
+                <h2 className="text-6xl font-extrabold text-center mb-12 text-white drop-shadow-[0_0_20px_#00BFFF]">
+                    Domains
+                </h2>
 
-         <DomainsCarousel />
-         <Timeline />
-        {/* <Laptop/> */}
-        </div>
-        <SponsorsPage/>       
-{/* <BowlingEffect/> */}
-<PortalEffectSlider/>
+                <DomainsCarousel />
+                <Timeline />
+            </div>
+            {/* <SponsorsPage /> */}
+            <PortalEffectSlider />
 
         </div>
     );
